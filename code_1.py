30ab4124d93eaa70ba947483282cdfa55a6f4221
Qirrat098 <qirratazam098@gmail.com>
2025-07-30 16:43:12 +0500

    merged

--- code_1.py
+++ code_1.py
@@ -4,3 +4,8 @@ movies = pd.read_csv('tmdb_5000_movies.csv')
 credits = pd.read_csv('tmdb_5000_credits.csv')
 
 movies = movies.merge(credits, on='title')
+print(movies.shape)
+
+#genres, id, keywords, title, overview, cast, crew
+movies = movies[['movie_id','title','overview','genres','keywords','cast','crew']]
+print(movies.head(1))
