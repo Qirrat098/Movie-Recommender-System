9c5c916230fefd4f6589ffdb5d1ece700dea8f1a
Qirrat098 <qirratazam098@gmail.com>
2025-07-31 15:34:37 +0500

    - Merged 'credits' data into the movies DataFrame using the 'title' column.

--- code_1.py
+++ code_1.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import ast
+
 movies = pd.read_csv('tmdb_5000_movies.csv')
 credits = pd.read_csv('tmdb_5000_credits.csv')
 
@@ -12,3 +14,13 @@ print(movies.head(1))
 movies.isnull().sum()
 movies.dropna(inplace = True)
 movies.duplicated().sum()
+movies[movies.duplicated()]
+movies = movies.drop_duplicates()
+movies.duplicated().sum()
+movies.iloc[0].genres
+ast.literal_eval('[{"id": 28, "name": "Action"}, {"id": 12, "name": "Adventure"}, {"id": 14, "name": "Fantasy"}, {"id": 878, "name": "Science Fiction"}]')
+def convert(obj):
+    L = []
+    for i in ast.literal_eval(obj):
+        L.append(i['name'])
+    return L
